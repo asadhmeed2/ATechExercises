d6d80d5a291d369452d92b4b8fbb62d97122cb0f
newasad <asadhmeed2@gmail.com>
2023-11-23 20:02:26 +0200

    closures-modules / add ex1

---
+++ week2/closures-modules/ex1/main.js
@@ -0,0 +1,33 @@
+function StringFormatter (){
+    
+    
+    function capitalizeFirst(text){
+        const lowerCaseText = text.toLowerCase();
+        const result = lowerCaseText[0].toUpperCase() + lowerCaseText.substr(1)
+
+        return result
+    } 
+
+    function toSkewerCase(text){
+        let result = '';
+
+        for(let char of text){
+            if(char === " "){
+                result += '-'
+            }else{
+                result += char
+            }   
+        }
+        return result;
+    } 
+
+    return {
+        toSkewerCase,
+        capitalizeFirst
+    }
+}
+
+const stringFormatter = StringFormatter()
+
+console.log(stringFormatter.capitalizeFirst('as Ad'));
+console.log(stringFormatter.toSkewerCase('as Ad'));
