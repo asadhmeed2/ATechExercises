a728df4f513625737d81604e31591b5c5f2df804
newasad <asadhmeed2@gmail.com>
2023-11-26 19:09:33 +0200

    jquery ex4

---
+++ week3/jQuery/ex4/main.js
@@ -0,0 +1,7 @@
+const cart = $('#cart')
+
+$("#items").on('click',".item", function(){
+    if($(this).data().instock){
+        cart.append(`<div class=cart-item>${$(this).text()}</div>`)
+    }
+})
