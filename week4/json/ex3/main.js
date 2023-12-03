533fdcddbad3c7ca00f37044d5de1941efb00a9a
newasad <asadhmeed2@gmail.com>
2023-12-03 15:18:19 +0200

    json ex3

---
+++ week4/json/ex3/main.js
@@ -0,0 +1,15 @@
+/*
+ anima
+
+ name, //string
+ weight { // object
+    number, // number
+    unit // string
+ }
+ habitat, // string
+isIndangered, //boolean
+foods [], // array
+hisPredators[], //array
+deliciousRate //number
+
+*/ 
