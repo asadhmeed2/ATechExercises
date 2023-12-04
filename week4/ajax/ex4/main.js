824101e0a87c0ec194ae2ba1a706f08066775a3e
newasad <asadhmeed2@gmail.com>
2023-12-04 17:19:06 +0200

    ajax ex4

---
+++ week4/ajax/ex4/main.js
@@ -0,0 +1,33 @@
+const API_KEY ="vGDiU1FUmxuSg5Jrk71FOkJPAbno5DlX" 
+
+const iframSurce = $('#ifram-template').html()
+
+const iframTemplate = Handlebars.compile(iframSurce);
+
+const results = $("#results") 
+
+const fetch = (topic)=>{
+    if(!topic  || typeof topic !== 'string' ){
+        return
+    }
+
+    $.ajax({
+        method: "GET",
+        url: `http://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${API_KEY}`,
+        success: function (data) {
+            
+            const newHtml =iframTemplate({src:data.data[0].embed_url})
+            results.append(newHtml)
+            
+            console.log(data.data[0].embed_url);
+        },
+        error: function (xhr, text, error) {
+            console.log(text);
+        }
+    }); 
+}
+
+const fetchIsbn = ()=>{
+ 
+    fetch('cats')
+}
