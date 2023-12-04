5fb794933f85bac41c6cca893f342476bbff8c8f
newasad <asadhmeed2@gmail.com>
2023-12-04 16:21:53 +0200

    ajax ex3

---
+++ week4/ajax/ex3/main.js
@@ -0,0 +1,32 @@
+const fetch = (queryType, queryValue)=>{
+    if(!queryType || !queryValue || typeof queryType !== 'string' || typeof queryValue !== 'string'){
+        return
+    }
+
+    $.ajax({
+        method: "GET",
+        url: `https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`,
+        success: function (data) {
+            const newData = data.items.map(book =>(
+                {
+                    authors: book.volumeInfo.authors,
+                    title : book.volumeInfo.title,
+                    
+                }))
+
+            console.log(data);
+        },
+        error: function (xhr, text, error) {
+            console.log(text);
+        }
+    }); 
+}
+
+const isbnIpt = $("#input")
+
+const fetchIsbn = ()=>{
+    const queryType ="isbn" //ISBN or title
+    const queryValue = "9789814561778"
+
+    fetch(queryType, queryValue)
+}
