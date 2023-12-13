3e7844ed2dd40971f270f5d1b67a6676c1698f1e
newasad <asadhmeed2@gmail.com>
2023-12-13 16:11:06 +0200

    express ex5

--- week5/express-intro/furniture-store/dist/main.js
+++ week5/express-intro/furniture-store/dist/main.js
@@ -1,6 +1,7 @@
 
 const view = $('#display');
 const input = $('#input');
+const bayInput = $('#bay-input');
 
 const getPrice = function(){
     const name = input.val();
@@ -12,5 +13,20 @@ const getPrice = function(){
     $.get(`priceCheck/${name}`).then((data)=>{
         view.empty();
         view.append(`price : ${data.price}`);
+    })
+}
+
+const bay = function(){
+    const name = bayInput.val();
+
+    if(!name){
+        return;
+    }
+
+    $.get(`buy/${name}`).then((data)=>{
+        console.log(data);
+        const {item:{name,price,inventory}} = data;
+        view.empty();
+        view.append(`Congratulations, you’ve just bought ${name} for ${price}. There are ${inventory} left now in the store.`)
     })
 }
