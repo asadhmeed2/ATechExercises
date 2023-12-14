e3236c8486d4218a2613e7f2b0539ce53adfc512
newasad <asadhmeed2@gmail.com>
2023-12-14 23:08:33 +0200

    express crud ex5

--- week5/express-crud-router/exercises/server.js
+++ week5/express-crud-router/exercises/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const MIN_NUMBER_OF_WORDS = 1;
+const BAD_REQUEST_STATUS = 400;
+const NOT_FOUND_STATUS = 404;
+const SUCCESS_STATUS = 200;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -50,7 +53,7 @@ app.post('/addWord', function(req, res){
     }
     
     addWord(word);
-    
+
     return res.send({text: `Added ${word}`, currentCount: wordCeunter[word] })
 })
 
@@ -73,6 +76,24 @@ app.post('/addWordsFromSentence', function(req, res){
     }
     
     return res.send({text: `Added ${numOfNewWords} words, ${numOldWords} already existed`, currentCount: -1})
+})
+
+app.delete('/deleteWord/:word',function(req, res){
+    const word = req.params.word
+
+    const wordsCount = word.trim().split(' ').length
+
+    if(wordsCount > MIN_NUMBER_OF_WORDS){
+        return res.status(BAD_REQUEST_STATUS).send({error:'input should be one word only'})
+    }
+    
+    if(!wordCeunter[word]){
+        return res.status(NOT_FOUND_STATUS).send({error:"deleted word not found"})
+    }
+    
+    delete wordCeunter[word]
+
+    return res.status(SUCCESS_STATUS).send('success')
 })
 
 app.listen(PORT, function(){
