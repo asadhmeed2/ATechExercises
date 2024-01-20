12fe2a19cec38b3be8019ed68fbc4d982d6e2a4f
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-01-21 00:18:45 +0200

    ..

--- week9/unit-tests-with-jest/code.js
+++ week9/unit-tests-with-jest/code.js
@@ -1,9 +1,76 @@
-const add = function(a, b){
-    return a + b
+const add = function (a, b) {
+  return a + b;
+};
+
+const calculateHyp = function (num1, num2) {
+  return Math.sqrt(num1 * num1 + num2 * num2);
+};
+
+class ArrayManipulator {
+  manipulate(arr1, arr2) {
+    let obj = {};
+    for (let i in arr1) {
+      obj[arr1[i]] = arr2[i];
+    }
+    return obj;
+  }
 }
 
-const calculateHyp = function(num1,num2){
-    return Math.sqrt(num1 * num1 + num2 * num2)
+class Exercises {
+  isEven(n) {
+    if (typeof n !== "number" || isNaN(n)) {
+      throw new Error("n must be a number");
+    }
+
+    return n % 2 == 0 ? true : false;
+  }
+
+  removeAtLeastOne(arr) {
+    if (arr.length === 0) {
+      throw new Error("array should not be empty");
+    }
+
+    let numItemsToRemove = Math.floor(Math.random() * (arr.length - 1)) + 1;
+    arr.splice(0, numItemsToRemove);
+    return arr;
+  }
+
+  simplify(str) {
+    let symbols = ["!", "#", ".", ",", "'"];
+    if (typeof str !== "string") {
+      throw new Error("this method only accepts string");
+    }
+
+    return str
+      .split("")
+      .filter((c) => symbols.indexOf(c) == -1)
+      .join("");
+  }
+
+  validate(booleans) {
+    if (!booleans) {
+      return { error: "array cannot be undefined or null" };
+    }
+
+    let truesNum = 0;
+    let falseNum = 0;
+
+    for (let bool of booleans) {
+      if (typeof bool === "boolean") {
+        if (bool) {
+          truesNum++;
+        } else {
+          falseNum++;
+        }
+      }
+    }
+
+    if (truesNum === 0 && falseNum === 0) {
+      return { error: "Need at least one boolean" };
+    }
+
+    return truesNum > falseNum;
+  }
 }
 
-module.exports = {add,calculateHyp}
+module.exports = { add, calculateHyp, ArrayManipulator, Exercises };
--- week9/unit-tests-with-jest/code.test.js
+++ week9/unit-tests-with-jest/code.test.js
@@ -1,12 +1,71 @@
-const {add,calculateHyp} = require('./code')
+const { add, calculateHyp, ArrayManipulator, Exercises } = require("./code");
+
+const exercises = new Exercises();
 
 test("add should return sum of a + b", () => {
-	    let sum = add(1, 2)
-	    expect(sum).toBe(3)
-})
+  let sum = add(1, 2);
+  expect(sum).toBe(3);
+});
 
 test("calculateHyp should return the square root of num1 + num2", () => {
-	    let sqrt = calculateHyp(3, 4)
-	    expect(sqrt).toBe(5)//
-})
+  let sqrt = calculateHyp(3, 4);
+  expect(sqrt).toBe(5); //
+});
 
+test("ArrayManipulator.manipulate()", () => {
+  const manipulator = new ArrayManipulator();
+  let x = ["food", "item", "location"];
+  let y = ["cherry", "lightbulb", "Tazmania"];
+
+  const results = manipulator.manipulate(x, y);
+  expect(results).toEqual({
+    food: "cherry",
+    item: "lightbulb",
+    location: "Tazmania",
+  });
+});
+
+// ex1
+test("isEven should return true if num is even", () => {
+  const isEven = exercises.isEven(4);
+
+  expect(isEven).toBeTruthy();
+  expect(() => exercises.isEven([])).toThrow();
+});
+
+//ex2
+test("remove at least one element from the array", () => {
+  const array = [4, 2, 5];
+  const modifiedArray = exercises.removeAtLeastOne([...array]);
+
+  expect(modifiedArray.length).toBeLessThan(array.length);
+  expect(() => exercises.removeAtLeastOne([])).toThrow();
+});
+
+//ex3
+test(`simplify should return a clean string without these symbols: "!", "#", ".", ",", "'"`, () => {
+  const complected = "'a!s.,ad'#";
+
+  const simple = exercises.simplify(complected);
+
+  expect(simple).toBe("asad");
+  expect(() => exercises.simplify(5)).toThrow();
+});
+
+//ex4
+test("validate should verify that there is at least on boolean in array", () => {
+  const truesMore = [true, false, true];
+  const empty = [];
+  const falsesMore = [false, false, true];
+  const undefinedProp = undefined;
+
+  const isTrue = exercises.validate(truesMore);
+  const emptyError = exercises.validate(empty);
+  const isFalse = exercises.validate(falsesMore);
+  const isUndefined = exercises.validate(undefinedProp);
+
+  expect(isTrue).toBeTruthy();
+  expect(emptyError).toEqual({ error: "Need at least one boolean" });
+  expect(isFalse).toBeFalsy();
+  expect(isUndefined).toEqual({ error: "array cannot be undefined or null" });
+});
