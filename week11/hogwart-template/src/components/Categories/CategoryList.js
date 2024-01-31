02bd6584b144a786143c6940a1d607e67d681efc
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-01-31 11:58:21 +0200

    ..

--- week11/hogwart-template/src/components/Categories/CategoryList.js
+++ week11/hogwart-template/src/components/Categories/CategoryList.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import React from "react";
+import CategoryCard from "./CategoryCard";
+import { Link } from "react-router-dom";
 
 const CategoryList = ({ categories }) => {
   return (
     <div className="category-list">
-        {/* add link to each of these */}
-        <CategoryCard id="potions" categoryData={categories[0]} />
-        <CategoryCard id="charms" categoryData={categories[1]} />
+      {/* add link to each of these */}
 
-      {categories.slice(2).map(category => (
+      {categories.map((category) => (
+        <Link to={`/wiki/${category.routeLink}/`}>
           <CategoryCard categoryData={category} />
-        ))}
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryList;
--- week11/hogwart-template/src/components/Header/HogwartNavbar.js
+++ week11/hogwart-template/src/components/Header/HogwartNavbar.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './HogwartNavbar.css';
+import React from "react";
+import "./HogwartNavbar.css";
+import { Link } from "react-router-dom";
 
 const HogwartNavbar = () => {
   return (
     <nav className="navbar">
-        <div className="navbar-link">Home</div>
-        <div className="navbar-link">About</div>
+      <Link className="navbar-link" to="/">
+        Home
+      </Link>
+      <Link className="navbar-link" to="/about">
+        About
+      </Link>
     </nav>
   );
-}
-
+};
 
 export default HogwartNavbar;
