92abb5ed49a0028d089198c9ce3dfc79be3e50e5
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-02-06 12:11:43 +0200

    react life cycle  ex3

--- week12/react-lifecycle/src/components/Post.jsx
+++ week12/react-lifecycle/src/components/Post.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Post = ({ post }) => {
+export const Post = ({ post, isWinSmall }) => {
   return (
-    <div className="post">
+    <div className="post" style={isWinSmall ? { width: "100%" } : {}}>
       <div className="post-title">{post.title}</div>
       <div className="post-body">{post.body}</div>
     </div>
--- week12/react-lifecycle/src/components/Posts.jsx
+++ week12/react-lifecycle/src/components/Posts.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Post } from "./Post";
 
 export const Posts = ({ posts }) => {
+  const [isWinSmall, setIsWinSmall] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsWinSmall(window.innerWidth <= 700);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="posts-list">
       {posts?.map((post) => (
-        <Post key={post.id} post={post} />
+        <Post key={post.id} post={post} isWinSmall={isWinSmall} />
       ))}
     </div>
   );
