5a6fde8e91d1aec3b0995ed1d801edaf61bd1be7
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-02-21 15:45:27 +0200

    matrix ex2

--- week13/matrices-lesson-exercises-master/src/Matrix.js
+++ week13/matrices-lesson-exercises-master/src/Matrix.js
@@ -40,22 +40,24 @@ class Matrix {
       console.log(this.matrix[row][i]);
     }
   }
+
+  findCoordinate(value) {
+    for (let row = 0; row < this.matrix.length; row++) {
+      for (let col = 0; col < this.matrix[row].length; col++) {
+        if (this.matrix[row][col] === value) {
+          return { x: col, y: row };
+        }
+      }
+    }
+    return null;
+  }
 }
 
 //You can paste the code from the lesson below to test your solution
 
 let m = new Matrix(3, 4);
-m.print();
-//prints
-/*
-1       2       3       4
-5       6       7       8
-9       10      11      12
-*/
-
-m.alter(0, 0, m.get(1, 1));
-m.printColumn(0); //prints 6, 5, 9 (separate lines)
-m.printRow(0); //prints 6, 2, 3, 4 (separate lines)
+console.log(m.findCoordinate(12)); //prints {x: 3, y: 2}
+console.log(m.findCoordinate(7)); //prints {x: 2, y: 1}
 
 /* Do not remove the exports below */
 module.exports = Matrix;
